17f39521559fc580e923b274bc8625aa2a009f84
Hugo <hugomaiaalves@hotmail.com>
2017-10-19 20:37:00 +0100

    ficha2ex3

--- Ficha2/ex3/main.c
+++ Ficha2/ex3/main.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
+//constantes
+#define POND1 0.25
+#define POND2 0.35
+#define POND3 0.40
+
 int main(int argc, char** argv) {
-    puts("ola");
-    return (0);
+    int nota1, nota2, nota3;
+    float media;
+    
+    //Introducao das notas
+    puts("Introduza a primeira nota: ");
+    scanf("%d", &nota1);
+    puts("Introduza a segunda nota: ");
+    scanf("%d", &nota2);
+    puts("Introduza a terceira nota: ");
+    scanf("%d", &nota3);
+    
+    //Calculo da media
+    media = (nota1 * POND1) + (nota2 * POND2) + (nota3 * POND3);
+    
+    //Estrutura condicional
+    if (media >= 9.5) {
+        printf("O aluno esta aprovado com %.2f valores!\n", media);
+    } else {
+        printf("O aluno esta reprovado com %.2f valores!\n", media);
+    }
+    return 0;
 }
 
