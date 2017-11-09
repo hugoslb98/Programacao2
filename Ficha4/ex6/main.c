6dc46c5c1cdc4a835956f9541540319f46c5a259
Hugo <hugomaiaalves@hotmail.com>
2017-11-09 14:12:12 +0000

    ex6ficha4

--- Ficha4/ex6/main.c
+++ Ficha4/ex6/main.c
@@ -1,24 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: hugom
- *
- * Created on 9 de Novembro de 2017, 10:17
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+void limparBufferEntrada(){
+    char ch;
+    while((ch = getchar()) != '\n' && ch !=EOF);
+}
 
-    return (EXIT_SUCCESS);
+void fazerRetangulo(int linhas, int colunas, char caracter){
+    int i, j;
+    for(i = 1; i <= colunas; ++i){
+        for(j = 1; j <= linhas; ++j){
+            if(i > 1 & i < colunas & j > 1 & j < linhas){
+              printf(" "); 
+            }else{
+               printf("%c", caracter);
+            }
+        }
+        puts("");
+        }
+}
+
+
+int main(int argc, char** argv) {
+    int linhas, colunas;
+    char caracter;
+    
+    puts("Introduza as linhas: ");
+    scanf("%d", &linhas);
+    
+    puts("Introduza as colunas: ");
+    scanf("%d", &colunas);
+    
+    limparBufferEntrada();
+    puts("Introduza o caracter: ");
+    scanf("%c", &caracter);
+    limparBufferEntrada();
+    
+    fazerRetangulo(linhas, colunas, caracter);
+    
+    return 0;
 }
 
