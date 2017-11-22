6a331da4891f21ef12aa9617604c19e55feb23f1
Hugo <hugomaiaalves@hotmail.com>
2017-11-22 14:28:12 +0000

    ex3ficha6

--- Ficha6/ex3/main.c
+++ Ficha6/ex3/main.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "utils.h"
 #define TAM 20
 
-int main(int argc, char** argv) {
-     char palavra1[TAM];
-     char palavra2[TAM];
-     int alfab;
+int main(int argc, char** argv) 
+{
+    
+    char palavra1[TAM], palavra2[TAM];
+    int alfab;
      
-     puts("Insira uma palavra: ");
-     lerString(palavra1, TAM);
+    puts("Insira uma palavra: ");
+    lerString(palavra1, TAM);
      
-     puts("Insira outra palavra: ");
-     lerString(palavra2, TAM);
+    puts("Insira outra palavra: ");
+    lerString(palavra2, TAM);
      
-     alfab = strcmp(palavra1, palavra2);
+    alfab = strcmp(palavra1, palavra2);
      
-     if(alfab < 0){
-         printf("A palavra e: %s\n", palavra1);
-         printf("A palavra e: %s\n", palavra2);
-     }else{
-         printf("A palavra e: %s\n", palavra2);
-         printf("A palavra e: %s\n", palavra1);
-     }
+    if(alfab < 0)
+    {
+        printf("A palavra e: %s\n", palavra1);
+        printf("A palavra e: %s\n", palavra2);
+    }
+    else
+    {
+        printf("A palavra e: %s\n", palavra2);
+        printf("A palavra e: %s\n", palavra1);
+    }
      
     return 0;
 }
-
