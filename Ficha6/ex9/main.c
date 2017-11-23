bdeef5dbfd057dfec4c4255e37152544790bb69c
Hugo <hugomaiaalves@hotmail.com>
2017-11-23 14:20:13 +0000

    ex9ficha6

--- Ficha6/ex9/main.c
+++ Ficha6/ex9/main.c
@@ -1,24 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: hugom
- *
- * Created on 23 de Novembro de 2017, 11:09
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "utils.h"
+#define TAM 20
 
-/*
- * 
- */
 int main(int argc, char** argv) {
-
-    return (EXIT_SUCCESS);
+    char palavra1[TAM], palavra2[TAM], palavra3[1] = ",";
+    
+    puts("Insira uma palavra 1: ");
+    lerString(palavra1, TAM);
+    
+    puts("Insira uma palavra 2: ");
+    lerString(palavra2, TAM);
+    
+    strcat(palavra1,palavra3);
+    
+    printf("A palavra e: %s\n", palavra1);
+   
+    
+    return 0;
 }
 
